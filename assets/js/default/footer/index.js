43e6114b4559267a463181005663d69c31e0c596
Myprofilee <andrep13.id@gmail.com>
2022-12-24 10:21:52 +0700

    first commit

---
+++ assets/js/default/footer/index.js
@@ -0,0 +1,17 @@
+fetch('/assets/menu/home/')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#home");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
+
+fetch('/assets/menu/notification/')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#notification");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
