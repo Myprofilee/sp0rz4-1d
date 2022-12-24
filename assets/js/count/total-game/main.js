df04f21ff6ff00f9920c0e5fe438b09af603604d
Myprofilee <andrep13.id@gmail.com>
2022-12-24 22:31:37 +0700

    first commit

---
+++ assets/js/count/total-game/main.js
@@ -0,0 +1,1 @@
+document.getElementById("total").innerHTML = "1";
--- assets/js/default/footer/index.js
+++ assets/js/default/footer/index.js
@@ -15,3 +15,12 @@ fetch('/assets/menu/notification/')
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
+
+fetch('/assets/menu/search/')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#search");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
